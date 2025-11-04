2ebec3fbcd604a2d463be56d4592d0f43a237629
MGuenther-build <Protools1802@gmail.com>
2025-11-04 14:29:36 +0100

    SPA

--- main.py
+++ main.py
@@ -135,5 +135,5 @@ def wettervorhersage_3_Tage():
 
 
 if __name__ == "__main__":
-    #app.run(debug=True)
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=True)
+    #app.run(host='0.0.0.0', port=5000, debug=True)
